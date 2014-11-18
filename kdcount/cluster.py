83c15d6f44b29063887a85328cb0e2827c2b0b0d
Yu Feng <yfeng1@coma.hpc1.cs.cmu.edu>
2014-11-18 12:32:45 -0500

    accelerate cluster with sorting.
    (better memory access pattern)

--- kdcount/cluster.py
+++ kdcount/cluster.py
@@ -11,6 +11,7 @@ class fof(object):
         shmnext = utils.empty(len(data), dtype='intp')
         self.iterations = 0
         N = 1 
+        #print N
         while N > 0:
             N = self._once(shmnext, np)
             self.iterations = self.iterations + 1
@@ -48,26 +49,39 @@ class fof(object):
         else:
             p = [(tree, tree)]
 
+        #print 'p', len(p)
         ll = self.linking_length
         ops = [0]
         next = self.next
         shmnext[:] = self.next
 
         with utils.MapReduce(np=np) as pool:
-            def work(i):
-                n1, n2 = p[i]
-                operations = 0
-                for r, i, j in n1.enumiter(n2, ll):
-                    if len(r) == 0: continue
+            def work(iwork):
+                n1, n2 = p[iwork]
+                operations = [0]
+                def process(r, i, j):
+                    if len(r) == 0: return 
+#                    print iwork, 'len(r)', len(r)
         #            with pool.critical:
                     if True:
                         mask2 = shmnext[i] > next[j]
                         i = i[mask2]
                         j = j[mask2]
-                        shmnext[i] = next[j]
-                        operations += mask2.sum()
-                return operations 
+                        nj = next[j]
+                        arg = numpy.lexsort((i, nj))
+                        i = i[arg]
+                        nj = nj[arg]
+                        lasts = (i[1:] != i[:-1]).nonzero()[0]
+                        i = i[lasts]
+                        nj = nj[lasts]
+                        shmnext[i] = nj
+                        operations[0] += len(i)
+#                    print iwork, 'len(r)', len(i)
+                n1.enum(n2, ll, process, bunch=10000 * 8)
+#                print 'work', iwork, 'done'
+                return operations[0]
             def reduce(op):
+                #print ops[0]
                 ops[0] += op
 
             pool.map(work, range(len(p)), reduce=reduce)
--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -7,10 +7,11 @@ import numpy
 
 def test_p(ll):
     pos = numpy.load(os.path.join(os.path.dirname(__file__),
-        'TEST-A00_hodfit-small.npy'))
+        'TEST-A00_hodfit-big.npy'))
 
+    print pos.max(axis=0), pos.min(axis=0)
     dataset = cluster.dataset(pos, boxsize=1.0)
-    r = cluster.fof(dataset, ll)
+    r = cluster.fof(dataset, ll, np=None)
     mass = r.sum()
     center = r.center()
     print 'linking length', ll
@@ -19,6 +20,10 @@ def test_p(ll):
     print 'counts in mass', numpy.bincount(numpy.int32(mass))
     print 'center of most massive group', center[mass.argmax()]
 
-test_p(0.02)
+test_p(0.0001)
+test_p(0.001)
+test_p(0.002)
+test_p(0.005)
 test_p(0.01)
+test_p(0.02)
 test_p(0.1)
