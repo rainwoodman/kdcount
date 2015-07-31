3abc74af29b3e1f8255140a6b4fbe2b7971fdd20
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-07-31 15:36:02 -0700

    runtests.py use installed library.

--- runtests.py
+++ runtests.py
@@ -1,10 +1,15 @@
 import sys
 import os
 
-from numpy.testing import Tester
-sys.path.insert(0, os.path.abspath('.'))
-
 from sys import argv
+
+from numpy.testing import Tester
+
+if '-s' in sys.argv:
+    # -s for in tree building
+    sys.path.insert(0, os.path.abspath('.'))
+    print("Testing in tree")
+    argv.remove('-s')
 
 tester = Tester()
 r = tester.test(extra_argv=['-w', 'tests'] + argv[1:])
