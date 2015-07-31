2617e2d04f956614a9a3366ccebf2e8a8cad05de
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-07-31 16:10:29 -0700

    properly fail if tests raise error.

--- runtests.py
+++ runtests.py
@@ -5,13 +5,7 @@ from sys import argv
 
 from numpy.testing import Tester
 
-if '-s' in sys.argv:
-    # -s for in tree building
-    sys.path.insert(0, os.path.abspath('.'))
-    print("Testing in tree")
-    argv.remove('-s')
-
 tester = Tester()
 r = tester.test(extra_argv=['-w', 'tests'] + argv[1:])
-if not r:
+if r.failures or r.errors:
     raise Exception("Tests failed")
