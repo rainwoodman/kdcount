9a2c5956be086ec19c6cc8cf24bc18dfcc08e191
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-07-30 17:14:45 -0700

    Add a module for FOF and pair counting on spherical coordinates.

---
+++ kdcount/sphere.py
@@ -0,0 +1,23 @@
+from . import models
+from .correlate import RBinning
+import numpy
+
+class points(models.points):
+    def __init__(self, ra, dec, weights=None, boxsize=None, extra={}):
+        ra = ra * (numpy.pi / 180.)
+        dec = dec * (numpy.pi / 180.)
+        pos = numpy.empty(len(ra), dtype=(ra.dtype, 3))
+        pos[:, 2] = numpy.sin(dec)
+        r = numpy.cos(dec)
+        pos[:, 0] = numpy.sin(ra) * r
+        pos[:, 1] = numpy.cos(ra) * r
+
+        models.points.__init__(self, pos, weights, boxsize, extra)
+
+class AngularBinning(RBinning):
+    @property
+    def angular_centers(self):
+        return 2 * numpy.arcsin(self.centers * 0.5) * (180. / numpy.pi)
+    @property
+    def angular_edges(self):
+        return 2 * numpy.arcsin(self.edges * 0.5) * (180. / numpy.pi)
---
+++ tests/test_sphere.py
@@ -0,0 +1,25 @@
+from kdcount import sphere
+from kdcount import cluster
+from kdcount import correlate
+import numpy
+from numpy.testing import assert_equal, assert_allclose
+
+
+def test_cluster():
+    numpy.random.seed(1234)
+    dec = numpy.arcsin(numpy.random.uniform(-1, 1, size=10000)) / numpy.pi * 180
+    ra = numpy.random.uniform(0, 2 * numpy.pi, size=10000) / numpy.pi * 180
+
+    dataset = sphere.points(ra, dec)
+
+    r = cluster.fof(dataset, 0.00001, np=None)
+
+    assert r.N == len(dataset)
+
+    binning = sphere.AngularBinning(1.0, 10)
+
+    r = correlate.paircount(dataset, dataset, binning=binning)
+    assert_allclose( 
+    r.sum1, 
+    0.5 * numpy.diff(-numpy.cos(binning.angular_edges * numpy.pi / 180)) * len(ra) ** 2,
+    rtol=1e-2)
