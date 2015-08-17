fa89085ade6652055113e969d3eb77926587e7a8
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-08-16 19:46:14 -0700

    heavier tests for cluster.

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -1,25 +1,29 @@
 from kdcount import cluster
 import numpy
-from numpy.testing import assert_equal
+from numpy.testing import assert_equal, run_module_suite
 
 def test_cluster():
 
-    pos = numpy.linspace(0, 1, 100, endpoint=False).reshape(-1, 1)
+    pos = numpy.linspace(0, 1, 10000, endpoint=False).reshape(-1, 1)
     dataset = cluster.dataset(pos, boxsize=1.0)
 
-    r = cluster.fof(dataset, 0.008, np=None)
-    assert r.N == 100
-    assert (r.sum() == 1).all()
-    r = cluster.fof(dataset, 0.011, np=None)
+    r = cluster.fof(dataset, 1.1 / len(pos), np=0, verbose=True)
+    print r.N
     assert r.N == 1
-    assert (r.sum() == 100).all()
+    assert (r.sum() == len(pos)).all()
+
+    r = cluster.fof(dataset, 0.8 / len(pos), np=0, verbose=True)
+    assert r.N == len(pos)
+    assert (r.sum() == 1).all()
+
 
 def test_parallel():
-    pos = numpy.random.uniform(size=(10000, 2))
+    pos = numpy.random.uniform(size=(100000, 2))
     dataset = cluster.dataset(pos, boxsize=1.0)
-    r1 = cluster.fof(dataset, 0.01)
-    r2 = cluster.fof(dataset, 0.01, np=4)
+    r1 = cluster.fof(dataset, 0.03, verbose=True)
+    r2 = cluster.fof(dataset, 0.03, np=4, verbose=True)
 
     assert r2.N == r1.N
+    print r2.length
     assert_equal(r2.sum(), r1.sum())
 
