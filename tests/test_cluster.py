d84e1a64bf7b8c393f08cb3ace0dd94f5ba13a0c
Yu Feng <rainwoodman@gmail.com>
2016-01-29 23:22:48 -0800

    Cleanup test_cluster.py

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -7,26 +7,12 @@ def test_cluster():
     pos = numpy.linspace(0, 1, 10000, endpoint=False).reshape(-1, 1)
     dataset = cluster.dataset(pos, boxsize=1.0)
 
-    r = cluster.fof(dataset, 1.1 / len(pos), np=0, verbose=True)
+    r = cluster.fof(dataset, 1.1 / len(pos))
     assert r.N == 1
     assert (r.sum() == len(pos)).all()
 
-    r = cluster.fof(dataset, 0.8 / len(pos), np=0, verbose=True)
+    r = cluster.fof(dataset, 0.8 / len(pos))
     assert r.N == len(pos)
     assert (r.sum() == 1).all()
 
-
-def test_parallel():
-    # this is deprecated. fof is on longer paralle.
-    numpy.random.seed(1234)
-    pos = numpy.random.uniform(size=(100000, 2))
-    dataset = cluster.dataset(pos, boxsize=1.0)
-    r1 = cluster.fof(dataset, 0.03, np=0, verbose=True)
-    r2 = cluster.fof(dataset, 0.03, np=4, verbose=True)
-
-    assert r2.N == r1.N
-    assert_equal(r2.sum(), r1.sum())
-
-    assert r2.N == r3.N
-    assert_equal(r2.sum(), r3.sum())
 
