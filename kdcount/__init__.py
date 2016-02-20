630a10b23f56dd08946f560e095edeffb91f8a22
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2016-02-20 01:57:06 -0800

    Move enumiter out from cython.

--- kdcount/__init__.py
+++ kdcount/__init__.py
@@ -7,6 +7,22 @@ class KDNode(_core.KDNode):
         return ('KDNode(dim=%d, split=%d, size=%d)' % 
                 (self.dim, self.split, self.size))
 
+    def enumiter(self, other, rmax, bunch=100000):
+        """ cross correlate with other, for all pairs
+            closer than rmax, iterate.
+
+            for r, i, j in A.enumiter(...):
+                ...
+            where r is the distance, i and j are the original
+            input array index of the data.
+
+            This uses a thread to convert from KDNode.enum.
+        """
+
+        def feeder(process):
+            self.enum(other, rmax, process, bunch)
+        for r, i, j in makeiter(feeder):
+            yield r, i, j
 
 class KDTree(_core.KDTree):
     __nodeclass__ = KDNode
@@ -18,3 +34,40 @@ class KDAttr(_core.KDAttr):
     def __repr__(self):
         return ('KDAttr(input=%s)' % (str(self.input.shape)))
 
+import threading
+try:
+    import Queue as queue
+except ImportError:
+    import queue
+import signal
+def makeiter(feeder):
+    q = queue.Queue(2)
+    def process(*args):
+        q.put(args)
+    def wrap(process):
+        try:
+            feeder(process)
+        except Exception as e:
+            q.put(e)
+        finally:
+            q.put(StopIteration)
+    old = signal.signal(signal.SIGINT, signal.SIG_IGN)
+    t = threading.Thread(target=wrap, args=(process,))
+    t.start()
+    signal.signal(signal.SIGINT, old)
+    while True:
+        item = q.get()
+        if item is StopIteration:
+            q.task_done()
+            q.join()
+            t.join()
+            break
+        elif isinstance(item, Exception):
+            q.task_done()
+            q.join()
+            t.join()
+            raise item
+        else:
+            if len(item) == 1: item = item[0]
+            yield item
+            q.task_done()
--- kdcount/pykdcount.pyx
+++ kdcount/pykdcount.pyx
@@ -4,7 +4,7 @@ cimport numpy
 import numpy
 from cpython.mem cimport PyMem_Malloc, PyMem_Free, PyMem_Realloc
 from numpy cimport npy_intp
-from libc.stdint cimport uint64_t as npy_uint64
+from numpy cimport npy_uint64
 numpy.import_array()
 
 cdef extern from "kdtree.h":
@@ -159,22 +159,6 @@ cdef class KDNode:
             raise RuntimeError("Too many friend of friend iterations. This is likely a bug.");
         return buf
 
-    def enumiter(self, KDNode other, rmax, bunch=100000):
-        """ cross correlate with other, for all pairs
-            closer than rmax, iterate.
-
-            for r, i, j in A.enumiter(...):
-                ...
-            where r is the distance, i and j are the original
-            input array index of the data.
-
-            This uses a thread to convert from KDNode.enum.
-        """
-
-        def feeder(process):
-            self.enum(other, rmax, process, bunch)
-        for r, i, j in makeiter(feeder):
-            yield r, i, j
 
     def enum(self, KDNode other, rmax, process=None, bunch=100000, **kwargs):
         """ cross correlate with other, for all pairs
@@ -391,40 +375,3 @@ cdef class KDTree:
         # no need to call kd_free !
         PyMem_Free(self.ref)
 
-import threading
-try:
-    import Queue as queue
-except ImportError:
-    import queue
-import signal
-def makeiter(feeder):
-    q = queue.Queue(2)
-    def process(*args):
-        q.put(args)
-    def wrap(process):
-        try:
-            feeder(process)
-        except Exception as e:
-            q.put(e)
-        finally:
-            q.put(StopIteration)
-    old = signal.signal(signal.SIGINT, signal.SIG_IGN)
-    t = threading.Thread(target=wrap, args=(process,))
-    t.start()
-    signal.signal(signal.SIGINT, old)
-    while True:
-        item = q.get()
-        if item is StopIteration:
-            q.task_done()
-            q.join()
-            t.join()
-            break
-        elif isinstance(item, Exception):
-            q.task_done()
-            q.join()
-            t.join()
-            raise item
-        else:
-            if len(item) == 1: item = item[0]
-            yield item
-            q.task_done()
