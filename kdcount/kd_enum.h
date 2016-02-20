6a2cbef1a8115394fe533060c6f667261eb012e5
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2016-02-20 12:24:46 -0800

    node -> nodes

--- kdcount/kd_enum.h
+++ kdcount/kd_enum.h
@@ -1,36 +1,36 @@
-static int kd_enum_check(KDNode * node[2], double rmax2,
+static int kd_enum_check(KDNode * nodes[2], double rmax2,
         kd_enum_callback callback, void * data);
 /*
  * enumerate two KDNode trees, up to radius max.
  *
- * for each pair i in node[0] and j in node[1],
+ * for each pair i in nodes[0] and j in nodes[1],
  * if the distance is smaller than maxr,
  * call callback.
  * if callback returns nonzero, terminate and return the value
  * */
-int kd_enum(KDNode * node[2], double maxr,
+int kd_enum(KDNode * nodes[2], double maxr,
         kd_enum_callback callback, void * data) {
-    int Nd = node[0]->tree->input.dims[1];
+    int Nd = nodes[0]->tree->input.dims[1];
     double distmax = 0, distmin = 0;
     double rmax2 = maxr * maxr;
     int d;
-    double *min0 = kd_node_min(node[0]);
-    double *min1 = kd_node_min(node[1]);
-    double *max0 = kd_node_max(node[0]);
-    double *max1 = kd_node_max(node[1]);
+    double *min0 = kd_node_min(nodes[0]);
+    double *min1 = kd_node_min(nodes[1]);
+    double *max0 = kd_node_max(nodes[0]);
+    double *max1 = kd_node_max(nodes[1]);
     for(d = 0; d < Nd; d++) {
         double min, max;
         double realmin, realmax;
         min = min0[d] - max1[d];
         max = max0[d] - min1[d];
-        kd_realdiff(node[0]->tree, min, max, &realmin, &realmax, d);
+        kd_realdiff(nodes[0]->tree, min, max, &realmin, &realmax, d);
         distmin += realmin * realmin;
         distmax += realmax * realmax;
     }
     /*
     printf("%g %g %g \n", distmin, distmax, maxr * maxr);
-    print(node[0]);
-    print(node[1]);
+    print(nodes[0]);
+    print(nodes[1]);
     */
     if (distmin > rmax2 * 1.00001) {
         /* nodes are too far, skip them */
@@ -38,46 +38,46 @@ int kd_enum(KDNode * node[2], double maxr,
     }
     if (distmax >= rmax2) {
         /* nodes may intersect, open them */
-        int open = node[0]->size < node[1]->size;
-        if(node[open]->dim < 0) {
+        int open = nodes[0]->size < nodes[1]->size;
+        if(nodes[open]->dim < 0) {
             open = (open == 0);
         }
-        if(node[open]->dim >= 0) {
-            KDNode * save = node[open];
-            node[open] = save->link[0];
+        if(nodes[open]->dim >= 0) {
+            KDNode * save = nodes[open];
+            nodes[open] = save->link[0];
             int rt;
-            rt = kd_enum(node, maxr, callback, data);
+            rt = kd_enum(nodes, maxr, callback, data);
             if(rt != 0) {
-                node[open] = save;
+                nodes[open] = save;
                 return rt;
             }
-            node[open] = save->link[1];
-            rt = kd_enum(node, maxr, callback, data);
-            node[open] = save;
+            nodes[open] = save->link[1];
+            rt = kd_enum(nodes, maxr, callback, data);
+            nodes[open] = save;
             return rt;
         } else {
-            /* can't open the node, need to enumerate */
+            /* can't open the nodes, need to enumerate */
         }
     } else {
         /* fully inside, fall through,
          * and enumerate  */
     }
 
-    return kd_enum_check(node, rmax2, callback, data);
+    return kd_enum_check(nodes, rmax2, callback, data);
 }
 
-static int kd_enum_check(KDNode * node[2], double rmax2,
+static int kd_enum_check(KDNode * nodes[2], double rmax2,
         kd_enum_callback callback, void * data) {
     int rt = 0;
     ptrdiff_t i, j;
     int d;
-    KDTree * t0 = node[0]->tree;
-    KDTree * t1 = node[1]->tree;
+    KDTree * t0 = nodes[0]->tree;
+    KDTree * t1 = nodes[1]->tree;
     int Nd = t0->input.dims[1];
 
-    double * p0base = malloc(node[0]->size * sizeof(double) * Nd);
-    double * p1base = malloc(node[1]->size * sizeof(double) * Nd);
-    /* collect all node[1] positions to a continue block */
+    double * p0base = malloc(nodes[0]->size * sizeof(double) * Nd);
+    double * p1base = malloc(nodes[1]->size * sizeof(double) * Nd);
+    /* collect all nodes[1] positions to a continue block */
     double * p1, * p0;
     double half[Nd];
     double full[Nd];
@@ -91,14 +91,14 @@ static int kd_enum_check(KDNode * node[2], double rmax2,
     }
 
     /* no need to collect weight */
-    kd_collect(node[0], &t0->input, p0base);
-    kd_collect(node[1], &t1->input, p1base);
+    kd_collect(nodes[0], &t0->input, p0base);
+    kd_collect(nodes[1], &t1->input, p1base);
 
-    for (p0 = p0base, i = node[0]->start; 
-        i < node[0]->start + node[0]->size; i++) {
+    for (p0 = p0base, i = nodes[0]->start; 
+        i < nodes[0]->start + nodes[0]->size; i++) {
         endata.i = t0->ind[i];
-        for (p1 = p1base, j = node[1]->start; 
-             j < node[1]->start + node[1]->size; j++) {
+        for (p1 = p1base, j = nodes[1]->start; 
+             j < nodes[1]->start + nodes[1]->size; j++) {
             double r2 = 0.0;
             for (d = 0; d < Nd; d++){
                 double dx = p1[d] - p0[d];
