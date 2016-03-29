013a66a5345c9d9014fc720f4f3afb4b41b32468
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2016-03-29 16:20:23 -0700

    bump to 0.3.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ extensions = [
             )
         ]
 
-setup(name="kdcount", version="0.3.4",
+setup(name="kdcount", version="0.3.5",
       author="Yu Feng",
       author_email="rainwoodman@gmail.com",
       description="A slower KDTree cross correlator",
