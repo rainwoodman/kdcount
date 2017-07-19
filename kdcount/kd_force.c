1a635609671d3a76e840b69884bc2750a9bc5856
biwei <david19962@pku.edu.cn>
2017-07-18 17:48:48 -0700

    realdiff returns abs we need direction here.
    Signed-off-by: Yu Feng <rainwoodman@gmail.com>

--- kdcount/kd_force.c
+++ kdcount/kd_force.c
@@ -20,8 +20,14 @@ distance(KDTree * tree, double * x, double * y, double * dx)
     double r2 = 0;
     int Nd = tree->input.dims[1];
     int d;
+    double half;
     for(d = 0; d < Nd; d++) {
-        dx[d] = kd_realdiff(tree, x[d] - y[d], d);
+        dx[d] = y[d] - x[d];
+        if (tree->boxsize) {
+            half = 0.5 * tree->boxsize[d];
+            if (dx[d] > half) dx[d] = dx[d] - tree->boxsize[d];
+            else if (dx[d] < -half) dx[d] = dx[d] + tree->boxsize[d];
+        }
         r2 += dx[d] * dx[d];
     }
     return r2;
